79573a365d13aee9c1d16d2795be68e7121b3dda
Sylvester C. Reventhal Abelsen <cornelius.a.r@gmail.com>
2016-11-24 09:56:42 +0100

    latest visualization

--- visualisation.py
+++ visualisation.py
@@ -19,10 +19,10 @@ famousPath + "Donald Trump.jpg",
 famousPath + "Justin Bieber.jpg",
 famousPath + "Jim Carrey.jpg",
 famousPath + "Miley Cyrus.jpg",
-famousPath + "Jennifer Aniston.jpg",
+famousPath + "Christina Aguilera.jpg",
 famousPath + "Michelle Obama.jpg",
 famousPath + "Beyonce Knowles.jpg",
-famousPath + "Dronning Margrethe.jpg",
+famousPath + "Lady Gaga.jpg",
 famousPath + "Angelina Jolie.jpg",
 famousPath + "Oprah Winfrey.jpg",
 famousPath + "Britney Spears.jpg",
@@ -37,11 +37,11 @@ notFamousPath + "NotFamousW3.png",
 notFamousPath + "NotFamousW4.png",
 notFamousPath + "NotFamousW5.png",
 notFamousPath + "DavidBlookAlike.jpg",
-notFamousPath + "NotFamouse.jpg",
+notFamousPath + "KlookAlike.jpg",
 notFamousPath + "NotFamousLookAlike.jpg",
-notFamousPath + "BritneySpearslookAlike.jpg",
-notFamousPath + "JimCareylookAlike.jpg",
-notFamousPath + "RihannaLookAlike.jpg"
+notFamousPath + "NotSoFamouse.jpg",
+notFamousPath + "maleNotFamouse.jpg",
+notFamousPath + "StylistlookAlike.jpg"
 ]
 famousYes = "Y"
 famousNo = "N"
@@ -50,6 +50,9 @@ correctFamous = [[],[]]
 incorrectFamous = [[],[]]
 correctNotFamous = [[],[]]
 incorrectNotFamous = [[],[]]
+correct = {}
+incorrect = {}
+invalidRows = [5, 13, 15]
 
 with open('data/data.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -59,25 +62,40 @@ with open('data/data.csv', 'rb') as csvfile:
             firstLine = False
             print row
         else:
+            testId = int(row[0])
             responseTime = row[7]
             exposureDuration = row[5]
             answerFamous = row[1]
             imageName = row[6]
-            print exposureDuration
+            if(testId in invalidRows):
+                #print "skipping testId " + str(testId)
+                continue
+                
+            if(not correct.has_key(exposureDuration)):
+                correct[exposureDuration] = 0.0
+            if(not incorrect.has_key(exposureDuration)):
+                incorrect[exposureDuration] = 0.0
             if(famousPath + imageName) in imagePaths:
                 if answerFamous == famousYes:
                     correctFamous[0].append(exposureDuration);
                     correctFamous[1].append(responseTime);
+                    correct[exposureDuration] += 1.0
                 else:
                     incorrectFamous[0].append(exposureDuration);
                     incorrectFamous[1].append(responseTime);
+                    incorrect[exposureDuration] += 1.0
             elif(notFamousPath + imageName) in imagePaths:
                 if answerFamous == famousNo:
                     correctNotFamous[0].append(exposureDuration);
                     correctNotFamous[1].append(responseTime);
+                    correct[exposureDuration] += 1.0
+                    
                 else:
                     incorrectNotFamous[0].append(exposureDuration);
                     incorrectNotFamous[1].append(responseTime);
+                    incorrect[exposureDuration] += 1.0
+            else:
+                print "Image path not found: " + imageName
         
 plt.scatter(correctFamous[0], correctFamous[1])
 plt.ylabel('Response Time')
@@ -101,6 +119,40 @@ plt.scatter(incorrectNotFamous[0], incorrectNotFamous[1])
 plt.ylabel('Response Time')
 plt.xlabel('Exposure Duration')
 plt.title('Incorrect Not Famous')
-plt.show()
+plt.show() 
 
-print 1.0/60 
+correctAsArray = [[],[]]
+incorrectAsArray = [[],[]]
+
+for key in correct:
+    correctAsArray[0].append(key)
+    correctAsArray[1].append(correct[key])
+for key in incorrect:
+    incorrectAsArray[0].append(key)
+    incorrectAsArray[1].append(incorrect[key])
+    
+plt.scatter(correctAsArray[0], correctAsArray[1])
+plt.ylabel('Correct')
+plt.xlabel('Exposure Duration')
+plt.title('Overall Correct Answers')
+plt.show() 
+
+plt.scatter(incorrectAsArray[0], incorrectAsArray[1])
+plt.ylabel('Incorrect')
+plt.xlabel('Exposure Duration')
+plt.title('Overall Incorrect Answers')
+plt.show() 
+
+avoidDivisionByZero = 100.0
+correctOverNotCorrect = [[],[]]
+for key in correct:
+    correctOverNotCorrect[0].append(key)
+    correctOverNotCorrect[1].append((correct[key]+avoidDivisionByZero)/(incorrect[key]+avoidDivisionByZero))
+plt.scatter(correctOverNotCorrect[0], correctOverNotCorrect[1])
+plt.ylabel("Correct/Not correct")
+plt.xlabel("Exposure Duration")
+plt.title("Correct divided by incorrect answers")
+plt.show()
+print "\n"
+for key in correct:
+    print "exposure =", key + ", correct =", str(correct[key]) + ", incorrect =", incorrect[key]
